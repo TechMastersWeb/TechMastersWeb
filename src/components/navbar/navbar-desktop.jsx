2e0768b4f52b13f9ed2fedcb73e1f3241f569257
af01camacho <af01camacho@gmail.com>
2023-07-11 12:00:26 -0500

    Arreglo DarkMode

--- src/components/navbar/navbar-desktop.jsx
+++ src/components/navbar/navbar-desktop.jsx
@@ -4,16 +4,16 @@ function NavbarDesktop() {
   const DarkMode = true;
   return (
     <>
-      <nav className=" dark hidden text-black bg-white md:flex items-center justify-between px-10 shadow-md">
+      <nav className="hidden md:flex items-center justify-between px-10 shadow-md">
         <h2 className="font-bold text-xl">TechMastersWeb</h2>
         <ul className="p-5 flex gap-5">
-          <Link className="dark:hover:bg-white dark:hover:text-black hover:bg-black hover:text-white py-2 px-4  font-semibold font-thing">
+          <Link className=" hover:bg-black hover:text-white py-2 px-4  font-semibold font-thing">
             Home
           </Link>
-          <Link className="dark:hover:bg-white dark:hover:text-black hover:bg-black hover:text-white py-2 px-4  font-semibold font-thing">
+          <Link className=" hover:bg-black hover:text-white py-2 px-4  font-semibold font-thing">
             Services
           </Link>
-          <Link className="dark:hover:bg-white dark:hover:text-black hover:bg-black hover:text-white py-2 px-4  font-semibold font-thing">
+          <Link className=" hover:bg-black hover:text-white py-2 px-4  font-semibold font-thing">
             Projects
           </Link>
         </ul>
